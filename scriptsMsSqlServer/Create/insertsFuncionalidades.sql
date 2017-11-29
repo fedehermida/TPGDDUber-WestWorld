21c9d65fa4b679d3eb3884998adfffeab72deedc
nachosoto.93@gmail.com <nachosoto.93@gmail.com>
2017-11-29 03:19:48 -0300

    Inserts Funcionalidades

--- scriptsMsSqlServer/Create/insertsFuncionalidades.sql
+++ scriptsMsSqlServer/Create/insertsFuncionalidades.sql
@@ -1,1 +1,28 @@
-INSERT INTO WEST_WORLD.Fun
+USE GD2C2017;
+
+
+INSERT INTO WEST_WORLD.Funcionalidad values ('ABM Empresa')
+INSERT INTO WEST_WORLD.Funcionalidad values ('ABM Cliente');
+INSERT INTO WEST_WORLD.Funcionalidad values ('ABM Sucursal');
+INSERT INTO WEST_WORLD.Funcionalidad values ('ABM Rol');
+INSERT INTO WEST_WORLD.Funcionalidad values ('ABM Factura');
+INSERT INTO WEST_WORLD.Funcionalidad values ('Registro de Pago');
+INSERT INTO WEST_WORLD.Funcionalidad values ('Rendición de Facturas');
+INSERT INTO WEST_WORLD.Funcionalidad values ('Devoluciones');
+
+
+
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (2,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (3,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (4,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (5,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (6,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (7,1);
+INSERT INTO WEST_WORLD.Rol_Funcionalidad values (8,1);
+
+
+INSERT INTO WEST_WORLD.Rol values ('Administrador',1);
+INSERT INTO WEST_WORLD.Rol values ('Cobrador',1);
+
+INSERT INTO WEST_WORLD.Rol_Usuario values (1,1);
+
