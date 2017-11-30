0ae777780a0ad9f141132f4d72bcbbc512038bba
guidocohen <guidocohen@yahoo.com.ar>
2017-11-30 12:00:30 -0300

    abmRol version leo

--- PagoAgilFrba/AbmRol/rolABM.Designer.cs
+++ PagoAgilFrba/AbmRol/rolABM.Designer.cs
@@ -1,16 +1,16 @@
-ï»¿namespace PagoAgilFrba.AbmRol
+namespace PagoAgilFrba.AbmRol
 {
     partial class rolABM
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,148 +20,178 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.FuncionalidadAAgregar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NombreRol = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listaFuncionalidadrs = new System.Windows.Forms.ListView();
+            this.CrearOUpdatear = new System.Windows.Forms.Button();
+            this.Agregar = new System.Windows.Forms.Button();
+            this.ListaDeRoles = new System.Windows.Forms.DataGridView();
+            this.BuscarRol = new System.Windows.Forms.Button();
+            this.RolABuscar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Habilitado = new System.Windows.Forms.CheckBox();
+            this.LimpiarBtn = new System.Windows.Forms.Button();
+            this.QuitarBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ListaDeRoles)).BeginInit();
             this.SuspendLayout();
             // 
-            // label6
+            // FuncionalidadAAgregar
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(44, 44);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(23, 13);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Rol";
+            this.FuncionalidadAAgregar.FormattingEnabled = true;
+            this.FuncionalidadAAgregar.Location = new System.Drawing.Point(317, 44);
+            this.FuncionalidadAAgregar.Name = "FuncionalidadAAgregar";
+            this.FuncionalidadAAgregar.Size = new System.Drawing.Size(121, 21);
+            this.FuncionalidadAAgregar.TabIndex = 0;
             // 
-            // textBox2
+            // label1
             // 
-            this.textBox2.Location = new System.Drawing.Point(73, 41);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(136, 20);
-            this.textBox2.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funcionalidades";
             // 
-            // label7
+            // NombreRol
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(215, 44);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(84, 13);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "Funcionalidades";
+            this.NombreRol.Location = new System.Drawing.Point(119, 44);
+            this.NombreRol.Name = "NombreRol";
+            this.NombreRol.Size = new System.Drawing.Size(100, 20);
+            this.NombreRol.TabIndex = 2;
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(1532, 383);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(268, 35);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Crear";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rol";
             // 
-            // comboBox1
+            // listaFuncionalidadrs
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(305, 40);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(165, 21);
-            this.comboBox1.TabIndex = 6;
+            this.listaFuncionalidadrs.GridLines = true;
+            this.listaFuncionalidadrs.Location = new System.Drawing.Point(317, 84);
+            this.listaFuncionalidadrs.Name = "listaFuncionalidadrs";
+            this.listaFuncionalidadrs.Size = new System.Drawing.Size(121, 97);
+            this.listaFuncionalidadrs.TabIndex = 4;
+            this.listaFuncionalidadrs.UseCompatibleStateImageBehavior = false;
+            this.listaFuncionalidadrs.View = System.Windows.Forms.View.List;
             // 
-            // button2
+            // CrearOUpdatear
             // 
-            this.button2.Location = new System.Drawing.Point(1532, 189);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(268, 38);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Agregar Funcionalidad";
-            this.button2.UseVisualStyleBackColor = true;
+            this.CrearOUpdatear.Location = new System.Drawing.Point(363, 187);
+            this.CrearOUpdatear.Name = "CrearOUpdatear";
+            this.CrearOUpdatear.Size = new System.Drawing.Size(75, 23);
+            this.CrearOUpdatear.TabIndex = 5;
+            this.CrearOUpdatear.Text = "Crear";
+            this.CrearOUpdatear.UseVisualStyleBackColor = true;
+            this.CrearOUpdatear.Click += new System.EventHandler(this.Crear_Click);
             // 
-            // listView1
+            // Agregar
             // 
-            this.listView1.Location = new System.Drawing.Point(47, 84);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(298, 238);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.Agregar.Location = new System.Drawing.Point(445, 41);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(75, 23);
+            this.Agregar.TabIndex = 6;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.UseVisualStyleBackColor = true;
+            this.Agregar.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox3
+            // ListaDeRoles
             // 
-            this.textBox3.Location = new System.Drawing.Point(94, 400);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(251, 20);
-            this.textBox3.TabIndex = 9;
+            this.ListaDeRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListaDeRoles.Location = new System.Drawing.Point(93, 275);
+            this.ListaDeRoles.Name = "ListaDeRoles";
+            this.ListaDeRoles.Size = new System.Drawing.Size(427, 150);
+            this.ListaDeRoles.TabIndex = 7;
+            this.ListaDeRoles.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListaDeRoles_CellDoubleClick);
             // 
-            // label8
+            // BuscarRol
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(44, 403);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 13);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Nombre";
+            this.BuscarRol.Location = new System.Drawing.Point(230, 233);
+            this.BuscarRol.Name = "BuscarRol";
+            this.BuscarRol.Size = new System.Drawing.Size(139, 23);
+            this.BuscarRol.TabIndex = 8;
+            this.BuscarRol.Text = "Buscar";
+            this.BuscarRol.UseVisualStyleBackColor = true;
+            this.BuscarRol.Click += new System.EventHandler(this.BuscarRol_Click);
             // 
-            // dataGridView1
+            // RolABuscar
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(94, 442);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(251, 185);
-            this.dataGridView1.TabIndex = 11;
+            this.RolABuscar.Location = new System.Drawing.Point(122, 235);
+            this.RolABuscar.Name = "RolABuscar";
+            this.RolABuscar.Size = new System.Drawing.Size(97, 20);
+            this.RolABuscar.TabIndex = 9;
             // 
-            // button3
+            // label3
             // 
-            this.button3.Location = new System.Drawing.Point(964, 810);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(242, 36);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Buscar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Rol";
             // 
-            // button4
+            // Habilitado
             // 
-            this.button4.Location = new System.Drawing.Point(413, 200);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Habilitado.AutoSize = true;
+            this.Habilitado.Location = new System.Drawing.Point(119, 84);
+            this.Habilitado.Name = "Habilitado";
+            this.Habilitado.Size = new System.Drawing.Size(73, 17);
+            this.Habilitado.TabIndex = 11;
+            this.Habilitado.Text = "Habilitado";
+            this.Habilitado.UseVisualStyleBackColor = true;
             // 
-            // rolABM
+            // LimpiarBtn
             // 
-            this.ClientSize = new System.Drawing.Size(507, 644);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label6);
-            this.Name = "rolABM";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.LimpiarBtn.Location = new System.Drawing.Point(375, 233);
+            this.LimpiarBtn.Name = "LimpiarBtn";
+            this.LimpiarBtn.Size = new System.Drawing.Size(145, 23);
+            this.LimpiarBtn.TabIndex = 12;
+            this.LimpiarBtn.Text = "Limpiar";
+            this.LimpiarBtn.UseVisualStyleBackColor = true;
+            this.LimpiarBtn.Click += new System.EventHandler(this.LimpiarBtn_Click);
+            // 
+            // QuitarBtn
+            // 
+            this.QuitarBtn.Location = new System.Drawing.Point(445, 84);
+            this.QuitarBtn.Name = "QuitarBtn";
+            this.QuitarBtn.Size = new System.Drawing.Size(75, 23);
+            this.QuitarBtn.TabIndex = 13;
+            this.QuitarBtn.Text = "Quitar";
+            this.QuitarBtn.UseVisualStyleBackColor = true;
+            this.QuitarBtn.Click += new System.EventHandler(this.QuitarBtn_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(908, 452);
+            this.Controls.Add(this.QuitarBtn);
+            this.Controls.Add(this.LimpiarBtn);
+            this.Controls.Add(this.Habilitado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RolABuscar);
+            this.Controls.Add(this.BuscarRol);
+            this.Controls.Add(this.ListaDeRoles);
+            this.Controls.Add(this.Agregar);
+            this.Controls.Add(this.CrearOUpdatear);
+            this.Controls.Add(this.listaFuncionalidadrs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NombreRol);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FuncionalidadAAgregar);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ListaDeRoles)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,24 +199,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox FuncionalidadAAgregar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox NombreRol;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listaFuncionalidadrs;
+        private System.Windows.Forms.Button CrearOUpdatear;
+        private System.Windows.Forms.Button Agregar;
+        private System.Windows.Forms.DataGridView ListaDeRoles;
+        private System.Windows.Forms.Button BuscarRol;
+        private System.Windows.Forms.TextBox RolABuscar;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox Habilitado;
+        private System.Windows.Forms.Button LimpiarBtn;
+        private System.Windows.Forms.Button QuitarBtn;
     }
 }
